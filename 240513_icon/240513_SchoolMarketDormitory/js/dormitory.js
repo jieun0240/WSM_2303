0ae1eea345fe6e279f3db15cc1d2712138a4a798
jieun0240 <128780183+jieun0240@users.noreply.github.com>
2024-10-28 14:38:10 +0900

    기숙사 selection 클릭으로 페이지 이동

--- 240513_icon/240513_SchoolMarketDormitory/js/dormitory.js
+++ 240513_icon/240513_SchoolMarketDormitory/js/dormitory.js
@@ -6,12 +6,23 @@ const boarDiv = document.querySelector("#board");
 
 const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomNameDiv, boarDiv];
 
+// 셀렉션 세개
+const selectionItemDivs = document.getElementsByClassName("selection-item");
+
 // calendarDiv.style.display = "block";
 // selectionWashingmachineTimeDiv.style.display = "block";
 // selectionRoomNameDiv.style.display = "block";
 // boarDiv.style.display = "block";
 
 const setPage = (page) => {
+    //clear selection
+    for (const selectionItemDiv of selectionItemDivs) {
+        selectionItemDiv.classList.remove("select");
+    }
+    //select selection
+    selectionItemDivs[page-1].classList.add("select");
+
+
     // clear pagas
     pageDivs.forEach(pageDiv => {
         pageDiv.style.display = "none"; // 모든 페이지 안 보이게 처리
@@ -20,3 +31,5 @@ const setPage = (page) => {
     // show pagas
     pageDivs[page-1].style.display = "block";
 }
+
+setPage(1);
