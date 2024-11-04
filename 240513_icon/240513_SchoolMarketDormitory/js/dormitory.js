2f85de1c87e1280793622b2807ea7213b5dee146
jieun0240 <128780183+jieun0240@users.noreply.github.com>
2024-11-04 15:06:21 +0900

    기숙사 필요한 초기 데이터 요청 응답 => js 변수

--- 240513_icon/240513_SchoolMarketDormitory/js/dormitory.js
+++ 240513_icon/240513_SchoolMarketDormitory/js/dormitory.js
@@ -14,15 +14,48 @@ const selectionItemDivs = document.getElementsByClassName("selection-item");
 // selectionRoomNameDiv.style.display = "block";
 // boarDiv.style.display = "block";
 
+let allData; //모든 초기화 정보 세탁기, 시간, 호실 정보
+let weeklyReservation; //미리 요일별로 예약된 정보
+let newResvation; //사용자가 입력하고 있는 예약 정보
+let reservations; //사용자가 예약 완료한 정보들
+
+const initData = async () => {
+    // allData 가져오기
+    const getAllData = async (url) => {
+        return fetch(url)
+        .then(response => response.json())
+        .then((data => {return data}))
+        .catch(error => console.error(error.message));
+    }
+
+    // weeklyReservation 가져오기
+    const getWeeklyReservation = async (url) => {
+        try {
+            const response = await fetch(url); // await 추가
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    // allData 호출 (구현 내용은 비어 있음)
+    const allData = await getAllData("js/allData.json"); // 비어있는 함수 호출
+    console.log(allData);
+    // weeklyReservation 호출
+    const weeklyReservation = await getWeeklyReservation("js/weekly-reservation.json");
+    console.log(weeklyReservation); // 주간 예약 데이터 출력
+}
+
 const setPage = (page) => {
     //clear selection
     for (const selectionItemDiv of selectionItemDivs) {
         selectionItemDiv.classList.remove("select");
     }
     //select selection
-    if(selectionItemDivs.length >= page) {  // 4page selection은 없음
-        selectionItemDivs[page-1].classList.add("select");
-    } 
+    if (selectionItemDivs.length >= page) {  // 4page selection은 없음
+        selectionItemDivs[page - 1].classList.add("select");
+    }
 
 
     // clear pagas
@@ -31,7 +64,8 @@ const setPage = (page) => {
     })
 
     // show pagas
-    pageDivs[page-1].style.display = "block";
+    pageDivs[page - 1].style.display = "block";
 }
 
+initData();
 setPage(1);
